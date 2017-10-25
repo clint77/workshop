ce2e50d4b3e4f3693b7664a020a990d15f466141
jeffrymorris <jeffrymorris@gmail.com>
2017-10-25 11:50:35 -0700

    Add support for vs2015 for the initial branch

---
+++ connect2017/developer/initial/dotnet/aspnetcore-vs2015/src/aspnetcore-vs2015/Models/DoctorAppointment.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace aspnetcore.Models
+{
+    public class DoctorAppointment
+    {
+        public string Type { get; set; }
+        [Required]
+        public string Doctor { get; set; }
+        [Required]
+        public string Patient { get; set; }
+        [Required]
+        public int Appointment { get; set; }
+        public long Timestamp { get; set; }
+    }
+}
---
+++ connect2017/developer/initial/dotnet/aspnetcore-vs2015/src/aspnetcore-vs2015/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Couchbase;
+using Couchbase.Authentication;
+using Couchbase.Configuration.Client;
+using Couchbase.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetcore_vs2015
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            services.AddSingleton<ICluster>(x =>
+            {
+                var cluster = new Cluster(new ClientConfiguration
+                {
+                    Servers = new List<Uri> { new Uri("http://localhost:8091") }
+                });
+                IAuthenticator auth = new PasswordAuthenticator("demo", "123456");
+                cluster.Authenticate(auth);
+                return cluster;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+    }
+}
