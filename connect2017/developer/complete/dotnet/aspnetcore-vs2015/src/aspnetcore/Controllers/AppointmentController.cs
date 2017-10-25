e3531d0f9e9b1b5b8284aaacae716d9283afe4e1
jeffrymorris <jeffrymorris@gmail.com>
2017-10-25 11:34:23 -0700

    Add VS2015 version of workshop

---
+++ connect2017/developer/complete/dotnet/aspnetcore-vs2015/src/aspnetcore/Controllers/AppointmentController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using aspnetcore.Models;
+using Couchbase;
+using Couchbase.Core;
+using Couchbase.N1QL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetcore.Controllers
+{
+    public class AppointmentController : BaseController
+    {
+        readonly IBucket _bucket;
+
+        public AppointmentController(ICluster cluster)
+        {
+            _bucket = cluster.OpenBucket("default");
+        }
+
+        [HttpGet("appointments")]
+        public async Task<IActionResult> Appointments()
+        {
+            var statement = $"SELECT META().id, `{_bucket.Name}`.* FROM `{_bucket.Name}` WHERE type = 'appointment'";
+            var query = QueryRequest.Create(statement);
+            var result = await _bucket.QueryAsync<DoctorAppointment>(query);
+            if (result.Success)
+                return Ok(result.Rows);
+            return CouchbaseError(result);
+        }
+
+        [ValidateModel]
+        [HttpPost("appointment")]
+        public async Task<IActionResult> Appointment([FromBody] DoctorAppointment appointment)
+        {
+            // type must be appointment, timestamp must be now, ignore/override any values posted in
+            appointment.Type = "appointment";
+            appointment.Timestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
+
+            var key = Guid.NewGuid().ToString();
+            var result = await _bucket.InsertAsync(new Document<DoctorAppointment>
+            {
+                Id = key,
+                Content = appointment
+            });
+            if (result.Success)
+                return Ok(appointment);
+            return CouchbaseError(result);
+        }
+
+        [HttpDelete("appointment")]
+        public async Task<IActionResult> Appointment(string appointmentid)
+        {
+            var n1ql = $"DELETE FROM `{_bucket.Name}` WHERE type = 'appointment' AND META().id = $id RETURNING *;";
+            var query = QueryRequest.Create(n1ql);
+            query.AddNamedParameter("id", appointmentid);
+            var result = await _bucket.QueryAsync<dynamic>(query);
+            if (result.Success)
+                return Ok(result.Rows);
+            return CouchbaseError(result);
+        }
+    }
+}
---
+++ connect2017/developer/complete/dotnet/aspnetcore-vs2015/src/aspnetcore/Models/Doctor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace aspnetcore.Models
+{
+    public class Doctor
+    {
+        public string Type { get; set; }
+        public DoctorInformation Information { get; set; }
+        [Required]
+        public string Department { get; set; }
+        public List<string> Patients { get; set; }
+        public long Timestamp { get; set; }
+    }
+
+    public class DoctorInformation
+    {
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string Gender { get; set; }
+    }
+}
